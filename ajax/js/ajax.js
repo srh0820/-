8284cec278a3addc1df2facf07ebcc3689f5495f
srh0820 <srh96820@gmail.com>
2024-06-18 11:24:08 +0900

    forEach 2단 카테고리

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -1,26 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function(){     
     $.ajax({
         url : "/ajax/json/data.json",
         type : "GET",
         dataType : "json",
         success : function(data){
             console.log(data, typeof data);
-            // JSON.parse(data) -> 자바스크립트가 쓸 수 있게 object 화
-            // $("#gnb").html(data["menu"][0]["category"])
-
+            // JSON.parse(data)  -> 자바스크립트가 쓸 수 있게 object
             let gnbli = "";
 
             data["menu"].forEach(function(ele, idx){
                 // 순서대로 순회하면서 실행
                 console.log(`${idx}번째 데이터는 ${ele}`)
-                gnbli += `<li>${ele["category"]}</li>`
+                gnbli +=`<li>${ele["category"]}`;
+                gnbli +=`<ul>`;
+                ele["subcategories"].forEach(function(eele, iidx){
+                    gnbli +=`<li>${eele["name"]}</li>`;
+                })
+                gnbli +=`</ul>`;
+                gnbli +=`</li>`
+                
             });
 
             $("#gnb").html(gnbli)
 
         },
         error : function(a, b, c){
-            console.log(a, b, c);
+           console.log(a, b, c);
         }
     })
 })
